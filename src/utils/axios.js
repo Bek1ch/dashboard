2b589a075b1a3b377aadcb628c74ef484d6882f1
Bek1ch <galymzhvnov@gmail.com>
2024-05-24 17:51:35 +0500

    Modified axios.js to include

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,20 +2,16 @@ import axios from "axios";
 import { TokenService } from "./token.service";
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
   baseURL: "http://89.219.23.53:7070/api",
   withCredentials: true,
-=======
-  baseURL: "/api"
 });
-
-axiosInstance.interceptors.request.use(function (config) {
-  const token = TokenService.getToken();
-  if (!token) return config;
-  config.headers.Authorization = `Bearer ${token}`;
-
-  return config;
->>>>>>> 0986afb2ba50c950543fdea7c24a1162775dca9c
-});
+//
+// axiosInstance.interceptors.request.use(function (config) {
+//   const token = TokenService.getToken();
+//   if (!token) return config;
+//   config.headers.Authorization = `Basic ${token}`;
+//
+//   return config;
+// });
 
 export { axiosInstance as axios };
